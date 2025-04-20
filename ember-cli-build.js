8d42c2870a23a23742064f24f11a45566de111ca
Patrick Kimber <code@pkimber.net>
2025-04-20 12:14:17 +0100

    'ember-polaris-pokedex' -  WarpDrive | Commit By Commit #2 + 'outdated'

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,12 @@ const { compatBuild } = require('@embroider/compat');
 
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
+  const { setConfig } = await import('@warp-drive/build-config');
   let app = new EmberApp(defaults, {});
+
+  setConfig(app, __dirname, {
+    compatWith: '99.0',
+  });
 
   return compatBuild(app, buildOnce);
 };
